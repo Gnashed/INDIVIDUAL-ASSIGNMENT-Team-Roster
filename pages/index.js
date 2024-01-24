7d373fa965a5faa58003050e4b90b508c22ecd85
Tion Blackmon <tionblackmon@icloud.com>
2024-01-23 21:19:16 -0600

    Snapshot

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+// import { Button } from 'react-bootstrap';
+// import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import PlayerCard from '../components/PlayerCard';
 
 function Home() {
   const { user } = useAuth();
@@ -15,11 +16,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <h1>Hello, {user.displayName}</h1>
+      <PlayerCard />
     </div>
   );
 }
